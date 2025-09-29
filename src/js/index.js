66933d32c39c5138ae709957cfecc3f6940e09d8
migiyubi <marugotoniboshi@gmail.com>
2025-09-29 20:24:31 +0900

    Add column indicating whether it's deep relic or not

--- src/js/index.js
+++ src/js/index.js
@@ -110,6 +110,11 @@ class App {
 
             const tr = document.createElement('tr');
 
+            const tdType = document.createElement('td');
+            tdType.classList.add('type');
+            tdType.classList.add(relicMaster.type);
+            tr.appendChild(tdType);
+
             const tdColor = document.createElement('td');
             tdColor.classList.add('color');
             tdColor.classList.add(relicMaster.color);
